1fde3f6c8e8c55c5e9f348c996caa9d447cb979a
André Barbosa Carneiro da Cunha Bauer <andrebb22@gmail.com>
2023-02-11 16:30:46 -0300

    removed storyboard

--- ArgyleLinkLight/ViewController.swift
+++ ArgyleLinkLight/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .yellow
+    }
 
 }
 
